fec273006ee179dd74adcbdf11fabc10d0cd226e
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-05-28 15:22:00 +0300

    fix: fixed issues

--- cinema/urls.py
+++ cinema/urls.py
@@ -2,16 +2,8 @@ from django.urls import path
 from cinema import views
 
 urlpatterns = [
-    path(
-        "cinema/movies/",
-        views.cinema_list,
-        name="cinema-list"
-    ),
-    path(
-        "cinema/movies/<int:pk>/",
-        views.cinema_detail,
-        name="cinema-detail"
-    ),
+    path("cinema/movies/", views.cinema_list, name="cinema-list"),
+    path("cinema/movies/<int:pk>/", views.cinema_detail, name="cinema-detail"),
 ]
 
 app_name = "cinema"
